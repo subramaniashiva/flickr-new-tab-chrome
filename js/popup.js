0050495241a3d4d48297418cf85d66549e80cc53
subramaniashiva <vengaishiva@gmail.com>
2015-07-13 23:51:32 +0530

    Pop up styling

--- js/popup.js
+++ js/popup.js
@@ -5,17 +5,27 @@ document.onreadystatechange = function() {
           defaultTag = 'No tag selected !';
       if(currentTag && currentTag !== 'null') {
         document.getElementById('currentTag').innerText = currentTag;
+      } else {
+        document.getElementById('currentTag').innerText = defaultTag;
       }
-      document.getElementById('currentTag').innerText = defaultTag;
       var submitButton = document.getElementById('submit');
       var inputTag = document.getElementById('tag');
       var clearButton = document.getElementById('clear');
+      var setTag = function(value) {
+        localStorage.setItem('flickrTag', value);
+        document.getElementById('currentTag').innerText = value;
+        localStorage.setItem('flickrRecrawl', 'true');
+        inputTag.value = '';
+      }
       submitButton.addEventListener('click', function() {
         if(inputTag.value) {
-          localStorage.setItem('flickrTag', inputTag.value);
-          document.getElementById('currentTag').innerText = inputTag.value;
-          localStorage.setItem('flickrRecrawl', 'true');
-          inputTag.value = '';
+          setTag(inputTag.value);
+        }
+      });
+      inputTag.addEventListener('keypress', function(event) {
+        console.log(event);
+        if(event.keyCode === 13 && inputTag.value) {
+          setTag(inputTag.value);
         }
       });
       clearButton.addEventListener('click', function() {
