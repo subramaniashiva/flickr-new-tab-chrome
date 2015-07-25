4ff3b91bd6af66b8dff07c8ea66e6e7353abd046
subramaniashiva <vengaishiva@gmail.com>
2015-07-25 19:52:00 +0530

    Background Js improved performance and code refactoring

--- js/background.js
+++ js/background.js
@@ -1,72 +1,46 @@
+var apiKey = '', currentTag, recrawl, 
+    prefetchImg, queryString,
+    displayedImages = 0;
+var settingsQuery = '&format=json&nojsoncallback=1&safe_search=1&content_type=1&sort=interestingness-desc&extras=url_l&per_page=300',
+    partialQueryTag = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + settingsQuery,
+    partialQueryGeneral = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=' + apiKey + settingsQuery;
+
+// AJAX crawling
+function callAjax(url, callback, params) {
+    var xmlhttp;
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            callback(xmlhttp.responseText, params);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
 // Function that process data from Flickr
 function processOutput(data) {
-        if (data) {
-            data = JSON.parse(data);
-            if (data.stat != 'fail' && data.photos.total !== "0") {
-                displayedImages = 0;
-                localStorage.setItem('displayedImages', 0);
-                localStorage.setItem('currentPage', data.photos.page);
-                localStorage.setItem('flickrData', JSON.stringify(data));
-                localStorage.setItem('totalPages', data.photos.pages);
-                localStorage.setItem('flickrRecrawl', 'false');
-                setNextPhoto(data);
-                return;
-            } else {
-                console.log('No data. Fall back to default');
-            }
-        } else {
-            console.log('Crawl error from Flickr');
-        }
-        // Since we have not received any data from Flickr,
-        // set flickrRecrawl to true
-        localStorage.setItem('flickrRecrawl', 'true')
-    }
-    // Get the image URL of item to be crawled
-function getImageUrl(photoObject) {
-        if (photoObject && photoObject.url_l) {
-            return photoObject.url_l;
-        }
-    }
-    // Gets the next image to be displayed
-function getNextPhotoIndex(dataObject) {
-        if (displayedImages < dataObject.photos.photo.length) {
-            displayedImages += 1;
-        } else {
+    if (data) {
+        data = JSON.parse(data);
+        if (data.stat != 'fail' && data.photos.total !== "0") {
             displayedImages = 0;
-            var currentPage = localStorage.getItem('currentPage');
-            if (currentPage && currentPage !== '') {
-                currentPage = parseInt(currentPage, 10);
-                if (currentPage < dataObject.photos.pages) {
-                    currentPage++;
-                } else {
-                    currentPage = 1;
-                }
-                localStorage.setItem('currentPage', currentPage);
-                localStorage.setItem('flickrRecrawl', 'true');
-            } else {
-                localStorage.setItem('currentPage', 1);
-            }
-        }
-        localStorage.setItem('displayedImages', displayedImages);
-        return displayedImages;
-    }
-    // Sets the details about next image to be crawled
-function setNextPhoto(dataObject) {
-    var nextPhotoIndex = getNextPhotoIndex(dataObject),
-        nextPhoto = dataObject.photos.photo[nextPhotoIndex];
-    if (nextPhoto && nextPhoto.height_l && nextPhoto.width_l && parseInt(nextPhoto.height_l) < parseInt(nextPhoto.width_l)) {
-        if (getImageUrl(nextPhoto) !== undefined && getImageUrl(nextPhoto) !== '' && getImageUrl(nextPhoto) !== 'undefined') {
-            localStorage.setItem('nextFlickrImage', getImageUrl(nextPhoto));
-            localStorage.setItem('nextOwner', nextPhoto.owner);
-            localStorage.setItem('nextId', nextPhoto.id);
+            localStorage.setItem('displayedImages', 0);
+            localStorage.setItem('flickrData', JSON.stringify(data));
+            localStorage.setItem('currentPage', data.photos.page);
+            localStorage.setItem('totalPages', data.photos.pages);
+            localStorage.setItem('flickrRecrawl', 'false');
+            setNextPhoto(data);
+            return;
         } else {
-            setNextPhoto(dataObject);
+            console.log('No data. Fall back to default');
         }
     } else {
-        setNextPhoto(dataObject);
+        console.log('Crawl error from Flickr');
     }
+    // Since we have not received any data from Flickr,
+    // set flickrRecrawl to true
+    localStorage.setItem('flickrRecrawl', 'true');
 }
-
+// This function sets the queryString along with the correct page number
 function setQueryString() {
     var pageNumber = localStorage.getItem('currentPage'),
         totalPages = localStorage.getItem('totalPages'),
@@ -88,50 +62,93 @@ function setQueryString() {
 
     // If there is no tag, set the query to crawl latest images from Flickr
     if (currentTag && currentTag !== 'null') {
-        queryString = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&tags=' + currentTag + '&format=json&nojsoncallback=1&safe_search=1&content_type=1&sort=interestingness-desc&extras=url_l&per_page=300&page=' + pageNumber;
+        queryString = partialQueryTag + '&tags=' + currentTag + '&page=' + pageNumber;
     } else {
-        queryString = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=' + apiKey + '&format=json&nojsoncallback=1&safe_search=1&content_type=1&sort=interestingness-desc&extras=url_l&per_page=300&page=' + pageNumber;
+        queryString = partialQueryGeneral + '&page=' + pageNumber;
     }
-
 }
-    // AJAX crawling
-function callAjax(url, callback, params) {
-    var xmlhttp;
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            callback(xmlhttp.responseText, params);
+// Function to set the query string and initiate crawl
+function recrawlFlickrData() {
+    setQueryString();
+    callAjax(queryString, processOutput);
+}
+// Get the image URL of item to be crawled
+function getImageUrl(photoObject) {
+    if (photoObject && photoObject.url_l) {
+        return photoObject.url_l;
+    }
+}
+// Gets the next image to be displayed
+function getNextPhotoIndex(dataObject) {
+    var currentPage;
+    if (displayedImages < dataObject.photos.photo.length) {
+        displayedImages += 1;
+    } else {
+        displayedImages = 0;
+        currentPage = localStorage.getItem('currentPage');
+        if (currentPage && currentPage !== '') {
+            currentPage = parseInt(currentPage, 10);
+            // Current page with all the results has beeen exhausted
+            // So set the next page
+            if (currentPage < dataObject.photos.pages) {
+                currentPage++;
+            } else {
+                currentPage = 1;
+            }
+            // Since we want a fresh page, set flickrRecrawl to be true
+            localStorage.setItem('currentPage', currentPage);
+            localStorage.setItem('flickrRecrawl', 'true');
+        } else {
+            localStorage.setItem('currentPage', 1);
         }
     }
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+    localStorage.setItem('displayedImages', displayedImages);
+    return displayedImages;
 }
-var apiKey = '',
-    currentTag, recrawl, prefetchImg, queryString,
-    displayedImages = 0;
+// Sets the details about next image to be crawled
+function setNextPhoto(dataObject) {
+    var nextPhotoIndex = getNextPhotoIndex(dataObject),
+        nextPhoto = dataObject.photos.photo[nextPhotoIndex],
+        nextImageUrl;
+    // Get only landscape photos
+    if (nextPhoto && nextPhoto.height_l && nextPhoto.width_l && 
+        parseInt(nextPhoto.height_l) < parseInt(nextPhoto.width_l)) {
+        nextImageUrl = getImageUrl(nextPhoto);
+        if (nextImageUrl && nextImageUrl !== 'undefined') {
+
+            localStorage.setItem('nextFlickrImage', getImageUrl(nextPhoto));
+            localStorage.setItem('nextOwner', nextPhoto.owner);
+            localStorage.setItem('nextId', nextPhoto.id);
+        } else {
+            setNextPhoto(dataObject);
+        }
+    } else {
+        setNextPhoto(dataObject);
+    }
+}
 
 if (localStorage.getItem('displayedImages')) {
     displayedImages = parseInt(localStorage.getItem('displayedImages'), 10);
-} else if (localStorage.getItem('flickrRecrawl') && localStorage.getItem('flickrRecrawl') === 'true') {
+} else if (localStorage.getItem('flickrRecrawl') && 
+    localStorage.getItem('flickrRecrawl') === 'true') {
+
     displayedImages = 0;
 }
 // This function is called from index.html
-// It sets the next item in the localStorage and requests for that image also
+// It sets the next item in the localStorage and requests for that image also.
 // So that it will be cached and when the next time the user opens a new tab, 
 // it will load fast
 function setNextItem() {
-    var recrawl, data, ajaxRequested = false;
+    var recrawl, data, nextImageUrl, ajaxRequested = false;
     
     recrawl = localStorage.getItem('flickrRecrawl');
     if (recrawl === null || recrawl === 'true') {
         console.log('recrawl happening');
-        //localStorage.setItem('currentPage', 1);
-        setQueryString();
-        callAjax(queryString, processOutput);
+        recrawlFlickrData();
         ajaxRequested = true;
-    } else if (localStorage.getItem('nextFlickrImage') === null || localStorage.getItem('nextFlickrImage') === 'undefined') {
-        setQueryString();
-        callAjax(queryString, processOutput);
+    } else if (localStorage.getItem('nextFlickrImage') === null || 
+        localStorage.getItem('nextFlickrImage') === 'undefined') {
+        recrawlFlickrData();
         ajaxRequested = true;
     }
 
@@ -141,15 +158,16 @@ function setNextItem() {
             data = JSON.parse(data);
             if (data.photos.photo.length != displayedImages) {
                 setNextPhoto(data);
-                if (localStorage.getItem('nextFlickrImage') && localStorage.getItem('nextFlickrImage') !== 'undefined') {
+                nextImageUrl = localStorage.getItem('nextFlickrImage');
+                if (nextImageUrl && nextImageUrl !== 'undefined') {
                     prefetchImg = new Image();
                     prefetchImg.src = localStorage.getItem('nextFlickrImage');
                 }
             } else {
-                callAjax(queryString, processOutput);
+                recrawlFlickrData();
             }
         } else {
-            callAjax(queryString, processOutput);
+            recrawlFlickrData();
         }
     }
 }
