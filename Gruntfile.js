67f5d44c3fbbb53ffee09e603b20352a5453bedd
subramaniashiva <vengaishiva@gmail.com>
2015-07-30 08:45:44 +0530

    Added gruntfile and tasks

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    src: '*.js',
+                    dest: 'build/scripts',
+                    cwd: 'js'
+                }]
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'build/css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        html_minify: {
+            build: {
+                files: [{
+                    expand: true,
+                    src: '*.html',
+                    dest: 'build/',
+                }]
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.loadNpmTasks('grunt-html-minify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['html_minify', 'uglify', 'cssmin']);
+
+};
